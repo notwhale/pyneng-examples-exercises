c2707a452b62e668edaf8bbfe088cbc8f20ae114
notwhale <notwhale@gmail.com>
2020-08-21 00:25:31 -0500

    Выполнено задание 4.3

--- exercises/04_data_structures/task_4_3.py
+++ exercises/04_data_structures/task_4_3.py
@@ -10,3 +10,17 @@
 """
 
 config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+
+"""
+
+Решение
+
+"""
+
+commands = config.split()
+vlans = commands[-1].split(',')
+print(vlans)
+
+# или
+
+print(config.split()[-1].split(','))
