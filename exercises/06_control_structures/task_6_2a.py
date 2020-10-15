22ec62f8a221e2c328b71ffe6e91a4ccc2a2d3ee
notwhale <notwhale@gmail.com>
2020-10-15 20:45:55 +0500

    task 6.2a completed

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Задание 6.2a
@@ -17,3 +18,36 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+# Решение
+
+ip_addr = input('Введите IP-адрес: ')
+
+ip = ip_addr.split('.')
+
+ip_valid = True
+
+if len(ip) != 4:
+    ip_valid = False
+else:
+    for i in ip:
+        if not i.isdigit():
+            ip_valid = False
+        else:
+            if int(i) < 0 or int(i) > 255:
+                ip_valid = False
+
+if not ip_valid:
+    print('Неправильный IP-адрес')
+else:
+    ip_oct1 = int(ip[0])
+    if 1 < ip_oct1 < 224:
+        print('unicast')
+    elif 223 < ip_oct1 < 240:
+        print('multicast')
+    elif ip_addr == '255.255.255.255':
+        print('local broadcast')
+    elif ip_addr == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
